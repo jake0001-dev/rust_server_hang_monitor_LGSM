c8667e4de883657086ca323b04adc1821a344b6d
Jake <59755030+jake0001-dev@users.noreply.github.com>
2023-04-25 03:05:41 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
+// Dependencies: webrconjs, axios, child_process
 const {exec} = require('child_process');
 var WebRcon = require('webrconjs')
 const axios = require('axios');
 
+// Variables
 const {server_ip, server_port, server_password, webhook_url} = require('./config.json');
 let connected = false;
-console.log(`
-    server_ip: ${server_ip}
-    server_port: ${server_port}
-    server_password: ${server_password}
-    webhook_url: ${webhook_url}
-`)
-const rcon = new WebRcon(server_ip, server_port);
-
-
 let lastMessage = null;
+
+const rcon = new WebRcon(server_ip, server_port);
 
 rcon.on('connect', () => {
     connected = true
@@ -37,9 +32,11 @@ rcon.on('message', function(message) {
         if (uptime > lastMessage) {
             lastMessage = uptime;
         } else {
+            try {
             axios.post(webhook_url, {
                 content: `Server ${server_ip}:${server_port} is down!`
             })
+
             exec('./rustserver restart', (err, stdout, stderr) => {
                 if (err) {
                     console.log(err);
@@ -48,6 +45,10 @@ rcon.on('message', function(message) {
                     console.log(stdout);
                 }
             })
+
+            } catch (error) {
+                console.log(error)
+            }
         }
     }
 })
@@ -56,18 +57,23 @@ rcon.on('message', function(message) {
 
 
 rcon.on('disconnect', () => {
-    axios.post(webhook_url, {
-        content: `Disconnected from ${server_ip}:${server_port}`
-    })
+    try {
+        axios.post(webhook_url, {
+            content: `Disconnected from ${server_ip}:${server_port}`
+        })
 
-    exec('./rustserver restart', (err, stdout, stderr) => {
-        if (err) {
-            console.log(err);
-            return;
-        } else {
-            console.log(stdout);
-        }
-    })
+        exec('./rustserver restart', (err, stdout, stderr) => {
+            if (err) {
+                console.log(err);
+                return;
+            } else {
+                console.log(stdout);
+            }
+        })
+    } catch (error) {
+        console.log(error)
+    }
+    
 })
 
 setInterval(() => {
